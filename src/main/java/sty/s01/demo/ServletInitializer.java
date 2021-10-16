ee40ca6fed991b50e2ffe3da1eb54e1dcd56f959
chlee2103 <rhdqnx@naver.com>
2021-10-16 20:23:40 +0900

    spring boot study 20211016 html 분리

--- src/main/java/sty/s01/demo/ServletInitializer.java
+++ src/main/java/sty/s01/demo/ServletInitializer.java
@@ -2,7 +2,7 @@ package sty.s01.demo;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+// web.xml이 없는 SpringBoot 웹 애플리케이션을 외부 Tomcat에서 동작하도록 하기 위해서
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
